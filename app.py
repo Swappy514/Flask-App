0956cb5c85ec39cce94161c9c654423c4690f09b
Swappy514 <swaroopjadhav5@gmail.com>
2025-07-21 09:45:00 +0530

    phase2: created data schema

--- app.py
+++ app.py
@@ -1,53 +1,31 @@
-from flask import Flask, request, redirect, url_for, session, Response
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.secret_key = "supersecret"
-
-# homepage login page
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        if username == "admin" and password == "123":
-            session["user"] = username  # store user in session
-            return redirect(url_for("Welcome"))
-        else:
-            return Response("Invalid credentials", mintype="text/plain")
-    return '''
-        <h2>Login Page</h2>
-        <form method="post">
-        Username: <input type="text" name="username"><br>
-        Password: <input type="password" name="password"><br>
-        <input type="submit" value="Login">
-        </form>
-    '''
-
-# welcome page after login
+    return render_template("login.html")
 
 
-@app.route("/welcome")
-def Welcome():
-    if "user" in session:
-        return f'''
-        <h2>Welcome, {session["user"]}!</h2>
-        <a href="{url_for('Logout')}">Logout</a>
-    '''
-    return redirect(url_for("Login"))
+@app.route("/submit", methods=["POST"])
+def submit():
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-# logout page
+    """" if username == "swappy!" and password == "514":
+        return render_template("welcome.html", name=username) """
 
+    valid_user = {
+        "swappy!": "514",
+        "admin": "123",
+        "user": "pass"
+    }
+    if username in valid_user and password == valid_user[username]:
+        return render_template("welcome.html", name=username)
+    else:
+        return "Invalid credentials"
 
-@app.route("/logout")
-def Logout():
-    session.pop("user", None)  # remove user from session
-    return redirect(url_for("login"))
-
-
-# ...existing code...
 
 if __name__ == "__main__":
     app.run(debug=True)
