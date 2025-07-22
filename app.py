c4a32a0d4b7ba24b23117b26793ed2930997c283
Swappy514 <swaroopjadhav5@gmail.com>
2025-07-22 10:00:00 +0530

    phase2: authentication logic

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+def student_profile():
+    return render_template(
+        "profile.html",
+        name="Swappy",
+        is_topper=True,
+        subjects=["Maths", "Science", "English"]
+    )
 
 
 if __name__ == "__main__":
