b716d13b31f2c12813332f2584c5caefa458cfef
Swappy514 <swaroopjadhav5@gmail.com>
2025-07-25 10:05:00 +0530

    phase4: connected database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,3 @@
-from flask import Flash
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from routes.auth import auth_bp
 
-db = SQLAlchemy
-
-def create_app():
-    app = Flash(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-    db.init_app(app)
-    return app
