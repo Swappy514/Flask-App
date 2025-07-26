75630e2ba9cbb0bcbe65a09ad21c174fbc6f659a
Swappy514 <swaroopjadhav5@gmail.com>
2025-07-26 14:00:00 +0530

    phase5: CI/CD config

--- app.py
+++ app.py
@@ -1,22 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.secret_key = "my_secret_key"
+db = SQLAlchemy()
 
-@app.route("/", methods=["GET", "POST"])
-def form():
-    if request.method == "POST":
-        name = request.form.get("name")
-        if not name:
-            flash("Name is required!")
-            return redirect(url_for("form"))
-        flash(f"Thank you, {name}!, your feedback was saved")
-        return redirect(url_for("thank_you"))
-    return render_template("form.html")
-
-@app.route("/thankyou")
-def thank_you():
-    return render_template("thank_you.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
-from flask import Flask
-from routes.auth import auth_bp
+from flask import Flash
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy
+
+def create_app():
+    app = Flash(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    db.init_app(app)
+    return app
